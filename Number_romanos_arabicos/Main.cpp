7609d9e6dea3a43e570a781e1c2deaa69b125c97
Fabian <arnolfabian0@gmail.com>
2022-10-03 21:18:06 -0500

    Cambios

--- Number_romanos_arabicos/Main.cpp
+++ Number_romanos_arabicos/Main.cpp
@@ -5,69 +5,44 @@
 
 using namespace std;
 
-int romanitos(char f[]) {
-    int i, valor;
-    int resultado = 0;
+class Calcular {
+private:
+    string numero_romano;
+public:
+    Calcular(string romann);
+    int Romanitos();
+};
 
-    for (i = 0; i < strlen(f); i++) {
+Calcular::Calcular(string romann) {
+    this-> numero_romano = romann;
+}
 
-        if (f[i] == 'I' && f[i + 1] == 'V' && strlen(f) == 2) {
-            resultado = 4;
-            break;
-        }
-        else if (f[i] == 'V' && f[i + 1] == 'I' && strlen(f) == 2) {
-            resultado = 6;
-            break;
-        }
-
-        if (f[i] == 'I' && f[i + 1] == 'X' && strlen(f) == 2) {
-            resultado = 9;
-            break;
-        }
-        else if (f[i] == 'X' && f[i + 1] == 'I' && strlen(f) == 2) {
-            resultado = 11;
-            break;
-        }
-
-        switch (f[i]) {
-        case 'I': valor = 1; break;
-            
-        case 'V': valor = 5; break;
-            
-        case 'X': valor = 10; break;
-            
-        case 'L': valor = 50; break;
-            
-        case 'C': valor = 100; break;
-            
-        case 'D': valor = 500; break;
-            
-        case 'M': valor = 1000; break;
-            
-        }
-
-            resultado += valor;   
+int Calcular::Romanitos() {
+    int value[100];
+    int n, i, number = 0;
+    char* r_num = const_cast <char*> (numero_romano.c_str());
+    n = strlen(r_num);
+    for (i = 0; i < n; i++) {
+        if (r_num[i] == 'I' || r_num[i] == 'i') value[i] = 1;
+        if (r_num[i] == 'V' || r_num[i] == 'v') value[i] = 5;
+        if (r_num[i] == 'X' || r_num[i] == 'x') value[i] = 10;
+        if (r_num[i] == 'L' || r_num[i] == 'l') value[i] = 50;
+        if (r_num[i] == 'C' || r_num[i] == 'c') value[i] = 100;
+        if (r_num[i] == 'D' || r_num[i] == 'd') value[i] = 500;
+        if (r_num[i] == 'M' || r_num[i] == 'm') value[i] = 1000;
     }
-    return resultado;
+    for (i = 0; i < n; i++) {
+        if (i == n - 1) number += value[i];
+        else if (value[i] >= value[i + 1]) number += value[i];
+        else number -= value[i];
+    }
+    return number;
 }
-
-
-
-void run() {
-    char cadena[20];
-    int decimal;
-
-    cout << "Escribe tu numero en romano: "; cin >> cadena;
-    decimal = romanitos(cadena);
-
-    cout << "\n El valor de su numero es:  " << decimal;
-}
-
-
 
 
 int main() {
-    int opcion;
+    int opcion, resultado;
+    string roman_number;
     do {
         cout << "Que quieres hacer? \n";
         cout << "--------------------------------- \n";
@@ -75,28 +50,38 @@ int main() {
         cout << "2 - Salir\n";
         cout << "---------------------------------- \n";
         cout << "Opcion: "; cin >> opcion;
+        
 
 
         switch (opcion) {
         case 1:
-            run();
+            cout << "---------------------------------- \n";
+            cout << "Digite el numero Romano: ";
+            cin >> roman_number;
+            Calcular num(roman_number);
+            resultado = num.Romanitos();
+            if (resultado > 0) {
+                /*
+                cout << "Numero romano ingresado: " << roman_number << endl;
+                */
+                cout << "---------------------------------- \n";
+                cout << "Su numero es: " << resultado << endl;
+                
+            }
+            else cout << "ERROR DIGITE UN NUMERO" << endl;
+
             cout << "\n---------------------------------------- \n";
             cout << "\nQuiere regresar al menu? Si = 1 No = 2 \n";
             cout << "Opcion: "; cin >> opcion;
+            cout << "\n---------------------------------------- \n";
             if (opcion == 1) {
                 break;
             }
             else {
                 return 0;
             }
-
-        case 2:
-            return 0;
-
-        default:
-            cout << "\nOpcion No disponible\n";
-            break;
         }
+
     } while (opcion != 2);
 
-}
+    }
